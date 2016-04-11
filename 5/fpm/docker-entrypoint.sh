6b49cc3ae9b7bcddd018898d72ef18aa1b2fd5dd
Sufijen Bani <sbani@users.noreply.github.com>
2016-04-11 11:06:52 +0200

    Activated and Fixed travis tests (#10)
    Activate travis again
    Override-cmd failed when running travis. Fixed it by removing echo in
    entrypoint.sh.
    
    Fixes #9

--- 5/fpm/docker-entrypoint.sh
+++ 5/fpm/docker-entrypoint.sh
@@ -6,13 +6,7 @@ if [[ "$1" == -* ]]; then
 	set -- php-fpm "$@"
 fi
 
-echo 'PHP init process starting ...'
-
 mkdir -p /var/www /usr/local/bin
 chown -R www-data:www-data /var/www /usr/local/bin
-
-echo
-echo 'PHP init process done. Ready for start up.'
-echo
 
 exec "$@"
