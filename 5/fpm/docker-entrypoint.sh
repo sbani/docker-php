32cb493954d09f80d94d5fc119dc57b08b5304fb
Sufijen Bani <sbani@users.noreply.github.com>
2017-08-28 15:38:05 +0200

    Add entrypoint loader (#38)
    This commit adds the ability to run entrypoint scripts added to the
    folder `/docker-entrypoint-initdb.d/*`. It will typically be used by
    adding script with volumes.
    
    It's a handy possibility to add multiple bash scripts.

--- 5/fpm/docker-entrypoint.sh
+++ 5/fpm/docker-entrypoint.sh
@@ -6,7 +6,18 @@ if [[ "$1" == -* ]]; then
 	set -- php-fpm "$@"
 fi
 
-mkdir -p /var/www /usr/local/bin
+logger "Fixing rights in container"
+mkdir -p /var/www
 chown -R www-data:www-data /var/www /usr/local/bin
+
+# docker-entrypoint-initdb.d, as provided by most official images allows for direct usage and extended images to
+# extend behaviour without modifying this file.
+for f in /docker-entrypoint-initdb.d/*; do
+    case "$f" in
+        *.sh)     logger "$0: running $f"; . "$f" ;;
+        "/docker-entrypoint-initdb.d/*") ;;
+        *)        logger "$0: ignoring $f" ;;
+    esac
+done
 
 exec "$@"
